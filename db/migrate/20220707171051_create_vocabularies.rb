c406b83823df23d0d407f2176e73e6fd4e37e11a
Ray Kuo <ray20140811@gmail.com>
2022-07-08 01:44:57 +0800

    add CRUD function

---
+++ db/migrate/20220707171051_create_vocabularies.rb
@@ -0,0 +1,11 @@
+class CreateVocabularies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :vocabularies do |t|
+      t.string :japanese
+      t.string :kanji
+      t.string :chinese
+
+      t.timestamps
+    end
+  end
+end
